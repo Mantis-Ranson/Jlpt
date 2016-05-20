1e78b0d58c627219f8d8622baeb357e32e876fdb
Mantis-Ranson <tyronestephenranson@gmail.com>
2016-05-20 11:16:56 +0900

    remove mistakes

--- server.js
+++ server.js
@@ -10,13 +10,7 @@ var config = {
   "domain":     process.env.OPENSHIFT_APP_DNS || '127.0.0.1',
   "serverip":   process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
   "serverport": process.env.OPENSHIFT_NODEJS_PORT || '8080',
-  "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080',
-  "protocol":   'ws://',
-  "wsclientopts": { reconnection: true, 
-                    reconnectionDelay: 2000,
-                    reconnectionAttempts: 100,
-                    secure: false
-                  }
+  "clientport": (process.env.OPENSHIFT_NODEJS_PORT) ? '8000':'8080'
 };
 
 server.listen(config.serverport, config.serverip, function() {
